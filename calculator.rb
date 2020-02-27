141e5362ffa08c8853da220310cd515441a63320
brave-ping-4201 <h_yazie@yahoo.com>
2020-02-27 03:43:31 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,7 +2,7 @@ describe "./calculator.rb" do
 
   it "contains a local variable called first_number that is assigned to a number" do
     first_number = get_variable_from_file('./calculator.rb', "first_number")
-    calculator.rb = "Hello World"
+    calculator.rb = 0.01
     expect(first_number).to be_an(Integer).or be_a(Float)
   end
   it "contains a local variable called second_number that is assigned to a number that isn't 0" do
